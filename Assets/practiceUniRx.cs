f730f0eb4bcfd9ba76fe506fe4c69bfb2173a6d6
Murakami <snowmanappli123@gmail.com>
2017-08-27 03:35:21 +0900

    空のフォルダをCreateRepositoryしたので、共有したかったunityProjectの内容をコピペ。

---
+++ Assets/practiceUniRx.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx; // 追加
+
+public class practiceUniRx : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Observable.Interval(System.TimeSpan.FromSeconds(1.0f))
+			.Subscribe(
+				count => { //OnNext
+					Debug.Log("count = " + count.ToString());
+				}
+				// ,e => { // OnError
+				// 	Debug.Log(e.Message);
+				// }
+				// ,() => { // OnCompleted
+				// 	Debug.Log("Completed!");
+				// }
+			);
+	}
+	
+	// Update is called once per frame
+	// void Update () {}
+}
---
+++ Assets/practiceUniRx2.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UniRx;
+using UniRx.Triggers;	
+
+public class practiceUniRx2 : MonoBehaviour
+, IPointerDownHandler
+, IPointerUpHandler 
+, IPointerClickHandler 
+// , IDragHandler
+// , IBeginDragHandler
+// , IEndDragHandler
+,ISelectHandler
+,IDeselectHandler
+
+{
+
+
+	Button button;
+
+	void Start()
+	{
+		button = GetComponent<Button>();
+		// ポインターアップで実行するやつ
+		button.OnPointerUpAsObservable()
+		.Subscribe( _ => {
+			Debug.Log("OnPointerUpしたよ");
+		});
+
+		// 毎秒実行するやつ
+		// Observable.Interval(System.TimeSpan.FromSeconds(1.0f))
+		// .Subscribe(
+		// 	count => { //OnNext
+		// 		Debug.Log("count = " + count.ToString());
+		// 	}
+		// 	// ,e => { // OnError
+		// 	// 	Debug.Log(e.Message);
+		// 	// }
+		// 	// ,() => { // OnCompleted
+		// 	// 	Debug.Log("Completed!");
+		// 	// }
+		// );
+	}
+
+//------------------------------------------------------------------
+// EventTriggerからよべるやつ
+//------------------------------------------------------------------
+	public void OnPointerUp(){
+		Debug.Log("OnPointerUpのイベントトリガー");
+	}
+
+//------------------------------------------------------------------
+// イベント系インターフェイスから呼べるやつ
+//------------------------------------------------------------------
+    public void OnPointerDown(PointerEventData eventData){
+        Debug.Log("OnPointerDown イベントです！");
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("IPointerUpHandler イベントです！");
+    }
+
+	public void OnPointerClick(PointerEventData eventData){
+        Debug.Log("OnPointerClick イベントです！");
+
+	}
+
+	// OnDragを使ってるとスクロールできなくなるので、使えない。
+	// OnDragを使ってないとOnBeginDrag, OnEndDrag も使えないしょうがない。
+	// public void OnDrag(PointerEventData eventData){
+    //     Debug.Log("OnDrag イベントです！");
+
+	// }
+	// public void OnBeginDrag(PointerEventData eventData){
+    //     Debug.Log("OnBeginDrag イベントです！");
+
+	// }
+
+	// public void OnEndDrag(PointerEventData eventData){
+    //     Debug.Log("OnEndDrag イベントです！");
+	// }
+	public void OnSelect(BaseEventData eventData){
+        Debug.Log("OnSelect イベントです！");
+
+	}
+	public void OnDeselect(BaseEventData eventData){
+        Debug.Log("OnDeselect イベントです！");
+
+	}
+
+}
