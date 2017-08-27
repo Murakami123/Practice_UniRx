fc0c13d495e2da4a352b6b54f22f4510f6de7485
Murakami <snowmanappli123@gmail.com>
2017-08-27 10:04:16 +0900

    更新があった場合自動でビルドするかテスト

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+//using UniRx;
+//using UniRx.Triggers;	
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// [SerializeField] 
+	// [System.NonSerialized] public ;
+	// [Header("ヘッダーです")] 
+
+//-----------------------------------------------
+// 処理
+//-----------------------------------------------
+	public void Initialize(){
+		Debug.Log("初期化メソッドです");
+	}
+
+//-----------------------------------------------
+// ボタンを押したときの処理
+//-----------------------------------------------
+	public void OnBtnTouch(){
+		Debug.Log("ボタンをタッチしました");
+	}
+}
